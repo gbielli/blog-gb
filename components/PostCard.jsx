bb84e17fef356dabaf815f8d39a72b510569735b
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-02-03 19:58:13 +0100

    build

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -10,6 +10,8 @@ const PostCard = ({post}) => {
 
 
   return (
+    <Link href={`/post/${post.slug}`} className="font-mulish" >
+
     <article className='grid grid-cols-2 lg:grid-rows-articles lg:grid-cols-1 gap-2'>
       <div className='overflow-hidden  max-h-[175px] md:max-h-[250px] w-full'>
         <img src={post.featuredImage.url}
@@ -19,9 +21,7 @@ const PostCard = ({post}) => {
       <div>
       <span className='font-mulish text-sm'> {moment(post.createdAt).format('DD MMM YYYY')}</span>
         <h3 className='text-lg font-bold cursor-pointer font-abril'>
-          <Link href={`/post/${post.slug}`} className="font-mulish" >
-            {post.title}
-          </Link> 
+            {post.title}       
         </h3>
         <div className='mt-1'>
         <p className='border-2 w-fit px-2 py-1 rounded-md border-primary mr-3 font-mulish text-primary hover:no-underline text-sm'>{post.categories[0].name}</p>
@@ -31,6 +31,7 @@ const PostCard = ({post}) => {
       </div>
       
     </article>
+    </Link> 
   )
 }
 
