bc0411f8a8194fd30da32c782d0e6c543c06f559
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-02-03 22:17:25 +0100

    modif svg

--- components/home/HomeSkills.jsx
+++ components/home/HomeSkills.jsx
@@ -13,33 +13,33 @@ const HomeSkills = () => {
       <div className='container mx-auto pb-20 px-6'>
         <HomeTitle title="Mes skills" description="Mes compétences" />
         <Dropdown  title="Web analytics" subtitle="Plus de 4ans">
-        <DropdownItem  src="/image/ga.svg" title="Google analytics" percentage="50%" />
-        <DropdownItem  src="/image/gtm.svg" title="Google tag manager" percentage="90%"  />
+        <DropdownItem  src="/image/ga.svg" title="Google analytics" percentage="90%" />
+        <DropdownItem  src="/image/gtm.svg" title="Google tag manager" percentage="95%"  />
         <DropdownItem  src="/image/gds.svg" title="Google data studio" percentage="80%" />
-        <DropdownItem  src="/image/matomo.svg" title="Matomo" percentage="85%" />
+        <DropdownItem  src="/image/Matomo.svg" title="Matomo" percentage="95%" />
         </Dropdown>
 
         <Dropdown  title="Marketing digital" subtitle="Plus de 4ans">
-        <DropdownItem  src="/image/semrush.png" title="SEO" percentage="90%" ratio="80%" />
-        <DropdownItem  src="/image/Ads.svg" title="SEA" percentage="90%"  />
-        <DropdownItem  src="/image/hubspot.svg" title="CRM" percentage="80%" />
+        <DropdownItem  src="/image/semrush.png" title="SEO" percentage="95%" />
+        <DropdownItem  src="/image/Ads.svg" title="SEA" percentage="80%"  />
+        <DropdownItem  src="/image/hubspot.svg" title="CRM" percentage="90%" />
         <DropdownItem  src="/image/make.svg" title="Automation" percentage="90%" />
-        <DropdownItem  src="/image/mailchimp.svg" title="Emailing" percentage="85%" />
+        <DropdownItem  src="/image/mailchimp.svg" title="Emailing" percentage="95%" />
         </Dropdown>
 
-        <Dropdown  title="Developpement front" subtitle="Plus de 4ans">
-        <DropdownItem  src="/image/html.svg" title="HTML" percentage="80%" />
+        <Dropdown  title="Developpement front" subtitle="Plus de 2ans">
+        <DropdownItem  src="/image/html.svg" title="HTML" percentage="90%" />
         <DropdownItem  src="/image/css.svg" title="CSS" percentage="90%" />
-        <DropdownItem  src="/image/javascript.svg" title="Javascript" percentage="80%" />
-        <DropdownItem  src="/image/react.svg" title="React" percentage="85%" />
-        <DropdownItem  src="/image/tailwind.svg" title="Tailwind" percentage="85%" />
+        <DropdownItem  src="/image/javascript.svg" title="Javascript" percentage="50%" />
+        <DropdownItem  src="/image/react.svg" title="React" percentage="50%" />
+        <DropdownItem  src="/image/tailwind.svg" title="Tailwind" percentage="90%" />
         </Dropdown>
 
         <Dropdown  title="Web design" subtitle="Plus de 4ans">
         <DropdownItem  src="/image/figma.svg" title="Figma" percentage="90%" />
-        <DropdownItem  src="/image/xd.svg" title="Adobe XD" percentage="90%" />
-        <DropdownItem  src="/image/illustrator.svg" title="Illustrator" percentage="8 %" />
-        <DropdownItem  src="/image/photoshop.svg" title="Photoshop" percentage="85%" />
+        <DropdownItem  src="/image/xd.svg" title="Adobe XD" percentage="80%" />
+        <DropdownItem  src="/image/illustrator.svg" title="Illustrator" percentage="80%" />
+        <DropdownItem  src="/image/photoshop.svg" title="Photoshop" percentage="90%" />
         </Dropdown>
       </div>
     </div>
