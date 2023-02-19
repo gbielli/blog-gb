1f367f4914c84fd552aa60de5b31e8cf35c4f724
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-02-19 18:48:02 +0100

    min w body

--- components/home/HomeAbout.jsx
+++ components/home/HomeAbout.jsx
@@ -11,7 +11,7 @@ const HomeAbout = () => {
       <p className='text-center font-mulish md:max-w-3xl mx-auto'>
       Aficionado du web et de ses nouveautés avec un solde capillaire négatif, je me suis spécialisé dans le web analytics, l'acquisition et l'expérience utilisateur.
       </p>
-      <div className='grid grid-cols-3 pb-20 pt-10'>
+      <div className='grid grid-cols-1 gap-6 sm:gap-0 sm:grid-cols-3 pb-20 pt-10'>
         <AboutItem number='04+' subtitle="années d'expériences" />
         <AboutItem number='29' subtitle="années sur terre" />
         <AboutItem number='~0' subtitle="cheveux sur le crâne" />
