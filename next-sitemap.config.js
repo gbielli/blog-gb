e5b6c10c265618738693c860a552b9b21734e1d2
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-02-22 21:26:51 +0100

    test sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://guillaumebielli.fr',
+    generateRobotsTxt: false, // (optional)
+    // ...other options
+  }
