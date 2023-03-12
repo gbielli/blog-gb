e9493fc172b0b2464a5c9ba8a7a6969c60398989
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-03-12 15:05:11 +0100

    change gtm module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React,  { useEffect, useState } from 'react';
 import { Layout } from '../components';
 import { getPosts } from '../services'
 import Script from 'next/script';
-
+import TagManager from 'react-gtm-module';
 
 import '../styles/globals.css';
 import 'tailwindcss/tailwind.css';
@@ -10,17 +10,13 @@ import type { AppProps } from 'next/app';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-TM94CJ5' });
+}, []);
+
   return (
-    <Layout>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TM94CJ5');
-      `}
-    </Script>
+    
+    <Layout>   
       <Component {...pageProps} />
     </Layout>
   )
