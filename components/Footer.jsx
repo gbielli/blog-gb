99f6032923c423dfc74e3b024c0aa2b4dabccd20
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-03-12 12:27:18 +0100

    h2 to p

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="footer bg-secondary">
             <div className="content-footer container mx-auto pt-20 pb-40 md:pt-40 md:pb-60 px-6">
                 <div className="content grid  grid-cols-1 md:grid-cols-2 gap-16">
-                    <h2 className='text-5xl font-abril'>Tu souhaites <br></br> <span id='underline'>échanger ?</span></h2>
+                    <p className='text-5xl font-abril'>Tu souhaites <br></br> <span id='underline'>échanger ?</span></p>
                     <p className='font-mulish self-center text-md'>Tu veux boire un café, discuter d'un projet, m'envoyer un mail sans raison particulière ? tu peux me contacter à l'adresse email <span id='underline'>hello@guillaumebielli.fr</span></p>
                 </div>
             </div>
