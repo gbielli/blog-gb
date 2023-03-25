f2744bf7a6e6682906cd70e48027baa1e9449761
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-03-25 14:22:20 +0100

    abril h4

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -135,7 +135,7 @@ const PostDetail = ( {post} ) => {
 
             h3: ({ children }) => <h3 className="before:mr-3 before:text-primary before:text-4xl text-3xl my-10 before:content-['#']" data-font='abril' id={slugify(children.props.content.map((item) => item.text))}>{children}</h3>,
 
-            h4: ({ children }) => <h4 className="text-2xl font-abril my-10" data-font='abril'>{children}</h4>,
+            h4: ({ children }) => <h4 className="text-2xl my-10" data-font='abril'>{children}</h4>,
 
             bold: ({ children }) => <strong className=''>{children}</strong>,
 
