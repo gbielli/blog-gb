4374ee38b002335f4d57778cdd72ed3cc2f2689a
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-06-15 12:15:55 +0200

    testjs

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,25 +1,25 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
-const handler = async (req, res) => {
+// const handler = async (req, res) => {
 
-  let url = `https://hook.eu1.make.com/c57kxsdczfsm0g72rprpjermmm22xp2e`;
+//   let url = `https://hook.eu1.make.com/c57kxsdczfsm0g72rprpjermmm22xp2e`;
 
-  try {
-    const response = await fetch(url, {
-      headers: {
-        Accept: 'application/json',
-      },
-    });
+//   try {
+//     const response = await fetch(url, {
+//       headers: {
+//         Accept: 'application/json',
+//       },
+//     });
 
-    if (!response.ok) {
-      res.status(response.status).json(response.statusText);
-    }
-    const data = await response.json();
+//     if (!response.ok) {
+//       res.status(response.status).json(response.statusText);
+//     }
+//     const data = await response.json();
 
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
+//     res.status(200).json(data);
+//   } catch (error) {
+//     res.status(500).json(error);
+//   }
+// };
 
-export default handler;
+// export default handler; 
