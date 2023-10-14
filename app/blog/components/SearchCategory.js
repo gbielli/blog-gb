7baee1177bfc0cd2960a8f62554c6d0a44062144
gbielli <guillaume.bielli@gmail.com>
2023-10-14 22:36:34 +0200

    SearchCategory

--- app/blog/components/SearchCategory.js
+++ app/blog/components/SearchCategory.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 const SearchCategory = ({ categories, isActive, setIsActive }) => {
 
-    
 
   return (
     <div className='button text-black flex flex-wrap gap-5 mb-10 md:mb-10 mt-10 px-6'>
