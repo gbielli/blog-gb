0b3a69c6652b170f75bd44145bf0cd623582f53d
gbielli <guillaume.bielli@gmail.com>
2023-10-31 23:02:52 +0100

    canonical articles

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -11,14 +11,19 @@ import { notFound } from "next/navigation"
 export async function generateMetadata({ params }) {
   const slug = params;
   const post = await getPost(slug);
+  
 
   if (!post) {
     return notFound()
   }
   
   return {
+    metadataBase: new URL('https://guillaumebielli.fr'),
     title:post.title,
     description:post.excerpt,
+    alternates: {
+      canonical: `/${post.slug}`
+    }
   }
 
 }
