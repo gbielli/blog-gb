37b85423e7c0b4fd0b47a7e5834c4e284126f06b
gbielli <guillaume.bielli@gmail.com>
2023-11-07 22:43:09 +0100

    project effect

--- app/projet/page.js
+++ app/projet/page.js
@@ -4,23 +4,50 @@ import LocomotiveScroll from '../utils/LocomotiveScroll';
 import Image from 'next/image';
 import {motion} from 'framer-motion'
 
+export const animation = {
+    initial: {y: "100%"},
+    enter: i => ({y: "0", transition: {duration: 0.75, ease: [0.85, 0, 0.15, 1],  delay: 0.095 * i}})
+  }
+
+export const opacity = {
+    initial:{opacity:0},
+    enter:{opacity:1,transition:{duration:0.4, delay:0.65}}
+}
+
+  const text = ["Parcours client", "Mypangee®"]
+
+  
+
 const page = () => {
   return (
     <LocomotiveScroll>
-    <div className='pt-20 px-6'>
-    <div className=' text-[10vw] leading-[10vw] font-clash'>Parcours client<span className='block'>Mypangee®</span></div>
-    <p className='text-2xl max-w-xl pt-10'>Refont complète du parcours client web de Mypangee, qui propose un parcours intégré par API en marque blanche sur les sites e-commerce de ses clients.</p>
-        <div class="tag mt-10 flex gap-3">
+    <div className='pt-10 px-6'>
+    {
+      text.map((word, index) => {
+    return <span className='overflow-hidden relative inline-flex leading-[17vw]' key={index}><motion.span className="text-[10vw] font-clash text-black leading-[10vw]" custom={index} variants={animation} initial="initial" whileInView={"enter"} viewport={{ once: true }} style={{ display: "inline-block" }}>{word} {index < word.length - 1 && <span>&nbsp;</span>} </motion.span></span>
+            
+        })
+        }
+    <motion.p variants={opacity} initial="initial" animate={"enter"} className='text-2xl max-w-xl pt-10'>Refont complète du parcours client web de Mypangee, qui propose un parcours intégré par API en marque blanche sur les sites e-commerce de ses clients.</motion.p>
+        <motion.div initial={{y:"70%", opacity:0}} animate={{y:0, opacity:1, transition: {duration: 1, ease: [0.83, 0, 0.17, 1], delay:0.4}}} class="tag mt-10 flex gap-3">
+      
             <p className='border-black border px-6 py-2 rounded-full inline-block text-xl'>UX/UI</p>
             <p className='border-black border px-6 py-2 rounded-full inline-block text-xl'>Analytics</p>
-        </div>
+        </motion.div>
         {/* <div className='h-[1px] bg-[#d4d4d4] w-full mt-20'></div> */}
 
         <div className='mt-20'>
-            <div className='flex justify-between mb-5'>
-                <p className='text-xl'>Parcours web</p>
-                <p className='text-xl'>2023</p>
+            <div className='overflow-hidden mb-5'>
+            <motion.div initial={{y:"100%"}} animate={{y:0, transition:{delay:1, ease: [0.83, 0, 0.17, 1], duration:0.8}}} className='flex justify-between relative'>
+                <div className='text-xl'>
+                    <p>Parcours web</p>
+                    </div>
+                <div className='text-xl'>
+                <p>2023</p>
+                </div>
+            </motion.div>
             </div>
+            
 
 
         <motion.div initial={{y:"70px", opacity:0}} animate={{y:"0", opacity:1, transition: {duration: 1, ease: [0.83, 0, 0.17, 1]}}} className='w-full'>
